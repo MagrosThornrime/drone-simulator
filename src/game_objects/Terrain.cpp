d4e22e057abce1f895f0c692ffbc99238141cdc6
MagrosThornrime <magros.thornrime@gmail.com>
2025-05-18 19:25:23 +0200

    Make bounding spheres for terrain parts smaller

--- src/game_objects/Terrain.cpp
+++ src/game_objects/Terrain.cpp
@@ -10,24 +10,22 @@ void Terrain::_fillCollider(AssetManager& assetManager)
     mesh.getVertices(vertices);
     mesh.getIndices(indices);
 
-    // find a y coordinate that is just slightly lower than the lowest point on terrain
-    float minY = std::numeric_limits<float>::infinity();
-    for (auto vertex : vertices)
-    {
-        minY = std::min(minY, vertex.y);
-    }
-    minY -= 0.000001f;
-
     // construct a pillar using the face and a triangle parallel to x and z axis.
     for (int i = 0; i < indices.size(); i += 3)
     {
         std::vector<glm::vec3> part;
+        float minY = std::numeric_limits<float>::infinity();
+        for (int j = 0; j < 3; j++)
+        {
+            glm::vec3 faceVertex = vertices[indices[i] + j];
+            minY = std::min(minY, faceVertex.y);
+            part.push_back(faceVertex);
+        }
         for (int j = 0; j < 3; j++)
         {
             glm::vec3 faceVertex = vertices[indices[i] + j];
             glm::vec3 lowerVertex = {faceVertex.x, minY, faceVertex.z};
             part.push_back(lowerVertex);
-            part.push_back(faceVertex);
         }
         _collider.addPart(part);
     }
